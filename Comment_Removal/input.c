9cedc522894d19040a05a4c459f5a4eb829cde41
Soumen Sen <144879914+soumensen411@users.noreply.github.com>
2026-09-06 02:49:12 +0000

    Organize lab projects into folders

--- Comment_Removal/input.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-int main() { 
-// This is a real comment 
-printf("This is not a comment: // inside string\n"); 
-printf("Multi-line style too: /* not a comment */\n"); 
-char c = '/';  // Another comment 
-return 0; 
-} 
--- Comment_Removal/output.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-int main() { 
-
-printf("This is not a comment: // inside string\n"); 
-printf("Multi-line style too: /* not a comment */\n"); 
-char c = '/';  
-return 0; 
-} 
